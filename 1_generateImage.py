d026a797d6b6ead6523ce890a4990f08a2135148
zhangxiaobin <zhangxiaobin@ipin.com>
2016-05-25 15:15:06 +0800

    add source files

---
+++ 1_generateImage.py
@@ -0,0 +1,35 @@
+# coding: utf-8
+
+import random
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+
+num = 1000
+'''
+generate images in ./lines/
+'''
+
+def genline(text, font, filename):
+    w, h = font.getsize(text)
+    image = Image.new('RGB', (w + 10, h + 10), 'white')
+    brush = ImageDraw.Draw(image)
+    brush.text((5, 3), text, font=font, fill=(0, 0, 0))
+    image.save(filename + '.jpg')
+    with open(filename + '.txt', 'w') as f:
+        f.write(text)
+        f.close()
+    pass
+
+if __name__ == '__main__':
+    for i in range(num):
+        fontname = './fonts/simfang.ttf'
+        fontsize = 24
+        font = ImageFont.truetype(fontname, fontsize)
+        #text = str(random.randint(1000000000, 9999999999))
+        #text = text + str(random.randint(1000000000, 9999999999))
+        text = str(random.randint(1000, 9999))
+        filename = './lines/' + str(i + 1)
+        genline(text, font, filename)
+    pass
+
---
+++ 2_dump.lua
@@ -0,0 +1,29 @@
+require 'image'
+require 'io'
+
+local num = 1000
+
+local fullset = {}
+fullset.size = num
+--local data = torch.Tensor(fullset.size, 3, 27, 142)
+local inputs = torch.Tensor(fullset.size, 27, 58)
+local targets = torch.Tensor(fullset.size, 4)
+
+for i = 1, num do
+    local filename = './lines/' .. tostring(i) .. '.jpg'
+    inputs[i] = image.load(filename, 1)
+
+    local file = io.open('./lines/' .. tostring(i) .. '.txt', 'r')
+    local labelstr = file:read()
+    local label = torch.IntTensor(4)
+    for j = 1, #labelstr do
+       label[j] = tonumber(string.sub(labelstr, j, j)) + 1
+    end
+    targets[i] = label
+end
+
+fullset.inputs = inputs
+fullset.targets = targets 
+
+print('saving data...')
+torch.save('fullset.dat', fullset)
